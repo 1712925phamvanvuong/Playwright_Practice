cc3f4495f11e7c2cf59578ea056f225ca2500f01
1712925phamvanvuong <phamvanvuongrn310@gmail.com>
2023-08-29 00:54:33 +0700

    try use locator by xpath

--- tests/first_scripts.spec.ts
+++ tests/first_scripts.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect, type Page} from '@playwright/test';
 
 test.beforeEach(async ({page}) => {
-    await page.goto('https://demo.playwright.dev/todomvc');
+    await page.goto('https://accounts.lambdatest.com/dashboard?_gl=1*o8ktup*_gcl_au*MTA2ODE0NzQ4Ni4xNjkzMTU1OTE1');
 });
 
 const TODO_ITEMS = [
@@ -14,36 +14,40 @@ const TODO_ITEMS = [
 test.describe('New Todo', () => {
     test('Delete Item In List', async ({page}) => {
         //Create a new todo locator
-        const newTodo = page.getByPlaceholder('What needs to be done?');
+        // const newTodo = page.getByPlaceholder('What needs to be done?');
 
-        //Create 1st todo
-        await newTodo.fill(TODO_ITEMS[0]);
-        await newTodo.press('Enter');
+        // //Create 1st todo
+        // await newTodo.fill(TODO_ITEMS[0]);
+        // await newTodo.press('Enter');
 
-        //Make sure item 1 is added
-        await expect(newTodo).toBeEmpty();
-        await expect(page.getByTestId("todo-title")).toHaveText([TODO_ITEMS[0]]);
-        //await checkNumberOfTodosInLocalStorage(page, 1);
+        // //Make sure item 1 is added
+        // await expect(newTodo).toBeEmpty();
+        // await expect(page.getByTestId("todo-title")).toHaveText([TODO_ITEMS[0]]);
+        // //await checkNumberOfTodosInLocalStorage(page, 1);
 
-        //Delete Item
-        await page.getByTestId("todo-title").hover();
-        await page.getByLabel("Delete").click();
+        // //Delete Item
+        // await page.getByTestId("todo-title").hover();
+        // await page.getByLabel("Delete").click();
+        //const newDashboard = page.locator('//span[@class=\'ltch-product-name\'][text()=\'Dashboard\']');
+
+         //Navigate Dashboard
+         await page.locator('//span[class=ltch-product-name][text()=Dashboard]').click();
     })
 
-    test('Clear complete', async ({page})=>{
-        const newTodo = page.getByPlaceholder('What needs to be done?');
+    // test('Clear complete', async ({page})=>{
+    //     const newTodo = page.getByPlaceholder('What needs to be done?');
 
-        //Create 1st todo
-        await newTodo.fill(TODO_ITEMS[0]);
-        await newTodo.press('Enter');
+    //     //Create 1st todo
+    //     await newTodo.fill(TODO_ITEMS[0]);
+    //     await newTodo.press('Enter');
 
-        //Make sure item 1 is added
-        await expect(newTodo).toBeEmpty();
-        await expect(page.getByTestId("todo-title")).toHaveText([TODO_ITEMS[0]]);
+    //     //Make sure item 1 is added
+    //     await expect(newTodo).toBeEmpty();
+    //     await expect(page.getByTestId("todo-title")).toHaveText([TODO_ITEMS[0]]);
 
-        //Select Item
-        await page.getByText()
-    })
+    //     //Select Item
+    //     await page.getByText()
+    // })
 })
 
 // async function checkNumberOfTodosInLocalStorage(page: Page, expected: number) {
