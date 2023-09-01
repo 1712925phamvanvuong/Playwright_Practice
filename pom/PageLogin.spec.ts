eefcd2bb7278aef502d6509230a7e33c5b54ecca
1712925phamvanvuong <phamvanvuongrn310@gmail.com>
2023-09-02 00:13:04 +0700

    Develop script for page login

--- pom/PageLogin.spec.ts
+++ pom/PageLogin.spec.ts
@@ -8,6 +8,8 @@ export default class PageLogin{
     buttonLogin: string = "//input[@value='Login']";
     fieldEmail: string = "//input[@id='input-email']";
     fieldPassword: string = "//input[@id='input-password']";
+    alertWarning(message: string){ return "//div[@class='alert alert-danger alert-dismissible'][text()='" + message + "']";}
+    buttonContinue: string = "//a[text()='Continue']";
 
     async inputUserName(email: string){
         await this.page.locator(this.fieldEmail).type(email);
@@ -24,5 +26,10 @@ export default class PageLogin{
         this.inputPassword(password);
         console.log('Click on button login')
         await this.page.locator(this.buttonLogin).click();
+    }
+
+    async verifyWaringIsDisplayed(message: string){
+        console.log('Verify warning alert is displayed')
+        await this.page.locator(this.alertWarning(message)).isVisible();
     }
 }
--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -2,19 +2,44 @@ import {chromium, test, expect, type Page} from '@playwright/test';
 import PageLogin from '../pom/PageLogin.spec';
 
 test.beforeEach(async ({page}) => {
-    const browsers = await chromium.launch();
-    const context = await browsers.newContext();
-    page = await context.newPage();
+    // const browsers = await chromium.launch();
+    // const context = await browsers.newContext();
+    // page = await context.newPage();
     await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/login');
 });
 
 test.describe('Test page login', ()=>{
     test('test login with user valid',async ({page}) => {
         const pageLogin = new PageLogin(page);
-        await pageLogin.login('abc@gmail.com','12345');
+        await pageLogin.login('starturetest@gmail.com','123458x@X');
     })
 
-    test('test login with user invalid',async (page) => {
+    test('test login with email and password is invalid',async ({page}) => {
+        const pageLogin = new PageLogin(page);
+        await pageLogin.login('test','abc');
+        await pageLogin.verifyWaringIsDisplayed(' Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.');
+    })
+
+    test('test login with email and password is empty',async ({page}) => {
+        const pageLogin = new PageLogin(page);
+        await pageLogin.login('','');
+        await pageLogin.verifyWaringIsDisplayed(' Warning: Your account has exceeded allowed number of login attempts. Please try again in 1 hour.');
+    })
+
+    test('test login with email is empty',async ({page}) => {
+        const pageLogin = new PageLogin(page);
+        await pageLogin.login('','123458x@X');
+        await pageLogin.verifyWaringIsDisplayed(' Warning: No match for E-Mail Address and/or Password.');
+    })
+
+    test('test login with password is empty',async ({page}) => {
+        const pageLogin = new PageLogin(page);
+        await pageLogin.login('abc@gmail','');
+        await pageLogin.verifyWaringIsDisplayed(' Warning: No match for E-Mail Address and/or Password.');
+    })
+
+    test('Create new customer',async ({page}) => {
+        const pageLogin = new PageLogin(page);
         
     })
 })
