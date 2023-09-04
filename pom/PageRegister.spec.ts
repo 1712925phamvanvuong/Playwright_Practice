bb3ea0a9b0547d5256ee722137dfbe3e3b0ccbef
1712925phamvanvuong <phamvanvuongrn310@gmail.com>
2023-09-04 23:23:23 +0700

    Update PageRegister

--- pom/PageRegister.spec.ts
+++ pom/PageRegister.spec.ts
@@ -1,14 +1,14 @@
-import{ expect , type Locator, type Page } from "@playwright/test";
+import{ expect , Locator, Page } from "@playwright/test";
+import {BaseTest} from "./BaseTest";
 
-export default class PageRegister{
-    subscribeYes: string = "//label[@for='input-newsletter-yes']";
-    subscribeNo: string = "//label[@for='input-newsletter-no']";
-    checkboxAgree: string = "//label[@for='input-agree']";
-    buttonContinue: string = "//input[@value='Continue']";
-    fieldInput(fieldName: string){ return "//input[@id='"+ fieldName + "']"};
+export default class PageRegister extends BaseTest{
+    readonly buttonContinue: Locator;
+    fieldInput(fieldName: string){ return "//input[@id='"+ fieldName + "']"; }
+    lableButon(lableName: string){ return "//label[@for='" + lableName + "']"; }
 
-    constructor (public page: Page){
-
+    constructor (page: Page){
+        super(page);
+        this.buttonContinue = page.locator("//input[@value='Continue']");
     }
 
     async registerUser(firstName: string, lastName: string, email: string, telephone: string, password: string, subscribe: boolean){
@@ -25,16 +25,16 @@ export default class PageRegister{
         console.debug('Input password confirm');
         await this.page.locator(this.fieldInput("input-confirm")).type(password);
         console.debug('Check subscribe default is No');
-        await this.page.locator(this.subscribeNo).isChecked();
+        await this.page.locator(this.lableButon("input-newsletter-no")).isChecked();
         if(subscribe){
             console.debug('Select subscribe yes');
-            await this.page.locator(this.subscribeYes).click();
+            await this.page.locator(this.lableButon("input-newsletter-yes")).click();
             console.debug('Check subscribe is Yes')
-            await this.page.locator(this.subscribeYes).isChecked();
+            await this.page.locator(this.lableButon("input-newsletter-yes")).isChecked();
         }
         console.debug('Check I agree policy');
-        await this.page.locator(this.checkboxAgree).click();
+        await this.page.locator(this.lableButon("input-agree")).click();
         console.debug('Click on continue button');
-        await this.page.locator(this.buttonContinue).click();
+        await this.buttonContinue.click();
     }
 }
