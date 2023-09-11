b2f7ef311e5580e3b887640992e6fd23a16e4fbf
1712925phamvanvuong <phamvanvuongrn310@gmail.com>
2023-09-12 00:01:40 +0700

    Try to code follow component

--- pom/PageHome.ts
+++ pom/PageHome.ts
@@ -3,12 +3,14 @@ import { BaseTest } from "./BaseTest";
 
 export class PageHome extends BaseTest{
     readonly searchButton: Locator
+    readonly registerButton: Locator
     navigationButton(button: string){
         return "//ul[@class='navbar-nav horizontal']//span[@class='title'][normalize-space()='" + button + "']";
     }
     constructor(page: Page){
         super(page);
         this.searchButton = page.locator("//button[text()='Search']");
+        this.registerButton = page.locator("//span[normalize-space()='Register']");
     }
     
     async verifySearchButtonSearchIsDisplayed(){
@@ -17,5 +19,10 @@ export class PageHome extends BaseTest{
 
     async login(){
         await this.page.locator(this.navigationButton("My account")).click();
+    }
+
+    async register(){
+        await this.page.locator(this.navigationButton("My account")).hover();
+        await this.registerButton.click();
     }
 }
--- tests/registerTest.spec.ts
+++ tests/registerTest.spec.ts
@@ -1,8 +1,12 @@
 import {test, expect, type Page} from '@playwright/test';
 import PageRegister from '../pom/PageRegister.spec';
+import { PageHome } from '../pom/PageHome';
 
 test.beforeEach(async ({page}) => {
-    await page.goto('https://ecommerce-playground.lambdatest.io/index.php?route=account/register');
+    let pageHome: PageHome;
+    pageHome = new PageHome(page);
+    await pageHome.goto('https://ecommerce-playground.lambdatest.io/');
+    await pageHome.register();
 });
 
 
