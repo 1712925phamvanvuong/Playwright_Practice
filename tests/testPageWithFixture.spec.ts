38658da768a0ca6c7a84281b7c9b507bbebbc009
1712925phamvanvuong <phamvanvuongrn310@gmail.com>
2024-09-20 23:10:26 +0700

    use tag filter in report

--- tests/testPageWithFixture.spec.ts
+++ tests/testPageWithFixture.spec.ts
@@ -9,16 +9,16 @@ test.beforeEach(async ({registerPage}) => {
 
 
 test.describe('Test page register', () => {
-    test('Test register user @smoke', async ({registerPage}) => {
+    test('Test register user', {tag: '@smoke'}, async ({registerPage}) => {
         await registerPage.registerUser(userdata[0].firstname, userdata[0].lastname, userdata[0].email, userdata[0].telephone, userdata[0].password, userdata[0].subscribe);
     })
 
-    test('Test register with user is existing', async ({registerPage}) => {
+    test('Test register with user is existing', {tag: '@regression'}, async ({registerPage}) => {
         await registerPage.registerUser(userdata[0].firstname, userdata[0].lastname, userdata[0].email, userdata[0].telephone, userdata[0].password, userdata[0].subscribe);
         await registerPage.verifyWarningIsDisplayed("Warning: E-Mail Address is already registered!");
     })
 
-    test('Test register with first name is empty',async ({registerPage}) => {
+    test('Test register with first name is empty', {tag: '@smoke'}, async ({registerPage}) => {
         await registerPage.registerUser(userdata[1].firstname, userdata[1].lastname, userdata[1].email, userdata[1].telephone, userdata[1].password, userdata[1].subscribe);
         await registerPage.verifyWarningIsDisplayed("Warning: E-Mail Address is already registered!");
     })
